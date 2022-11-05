33deee5248e4490330f28d3423ba119e10134180
Edwin Roman <romanseberino@gmail.com>
2022-11-05 09:59:07 -0400

    Legacy implementation

--- Data_Structure/C++/Tarea-linked-list/Lista copy.cpp
+++ Data_Structure/C++/Tarea-linked-list/Lista copy.cpp
@@ -1,18 +1,20 @@
 #include "Lista.h"
 #include <iostream>
 
-Lista::Lista(): _primer(NULL) , _ultimo(NULL), _lenght(0)
+
+Lista::Lista(): _primer(NULL) , _ultimo(NULL)
 {
 
 }
 
 void Lista::Agregar(Elemento* elemento)
 {
+
 	/*IMPLEMENTE ESTE METODO
 		Dado un objeto de tipo puntero a Elemento agréguelo a la lista.
-
 		Tenga pendiente que el atributo _primer guarda un puntero al primer elemento de la lista y que _ultimo es un puntero al al último elemento de la lista
 	*/
+
 	if(_primer == NULL){
 		
 		_primer = elemento;
@@ -24,8 +26,10 @@ void Lista::Agregar(Elemento* elemento)
 		_ultimo = elemento;
 	}
 
-	_lenght++;
+	
 }
+
+
 
 bool Lista::Eliminar(int indice)
 {
@@ -39,28 +43,94 @@ bool Lista::Eliminar(int indice)
 	*/
 
 	int inx = 0;
-		
-	if(_lenght-1 >= indice){
 
-		Elemento* elem = _primer;
+	Elemento* elem = _primer;
 
-		while (elem != NULL)
-		{
-			
-			Elemento* tmp = elem;
-			elem = elem->GetSiguiente();
-
-			if(inx == indice){
-				//delete elem;
-				std::cout << "deleted - true" << std::endl;
-				return true;
-			}
-
-			inx++;
-		}
-
+	// This while is to know how many element there are in the list.
+	while (elem != NULL)
+	{			
+		elem = elem->GetSiguiente();
+		inx++;
 	}
 
-	std::cout << "deleted - false" << std::endl;
-    return false;
+
+	if(!(inx-1 >= indice))
+		return false;
+	/* 
+		These two condition mean the same if the given index is higher than our 
+		calculated index we get out of range. (The under condition look more simple).
+	 */
+	if(indice > inx)
+		return false;
+
+
+
+	//Este bloque busca el elemento en espefico dado el indice.
+	inx = 0;
+	
+	Elemento* previous = NULL;
+	Elemento* current = _primer;
+
+	while (current != NULL)
+	{			
+		if(inx == indice){
+			break;
+		}else{
+
+			previous = current;
+			current = current->GetSiguiente();
+		}
+
+		inx++;
+	}
+	
+
+	if(current == NULL){
+		return false;
+	}else{
+		
+		if(previous != NULL){ 
+			previous->SetSiguiente(current->GetSiguiente());
+			delete current;
+		}
+		else{
+
+			if(_primer->GetSiguiente() != NULL){ 
+				_primer = _primer->GetSiguiente(); //Si estamos eliminando el primer elemento pero este tiene otro elementos al que apuntar.
+				delete current;
+			}else{
+				_primer = NULL; //Cuando queda solo ese primer elemento y nada a que apuntar.
+				delete current;
+			}
+
+		}
+		
+		return true;
+	}
+	
+
+	/* Refactor */
+
+	if(current == NULL)
+		return false;
+	
+
+	if(previous == NULL){ 
+		
+		if(_primer->GetSiguiente() == NULL){ 
+			_primer = NULL; //Cuando queda solo ese primer elemento y nada a que apuntar.
+			delete current;
+		}else{
+			_primer = _primer->GetSiguiente(); //Si estamos eliminando el primer elemento pero este tiene otro elementos al que apuntar.
+			delete current;
+		}
+	}
+	else{
+		previous->SetSiguiente(current->GetSiguiente());
+		delete current;
+	}
+		
+	return true;
+	 
 }
+
