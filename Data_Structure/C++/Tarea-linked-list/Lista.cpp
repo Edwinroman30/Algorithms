2e1ca7d3e04ea8bafc4e25ccccfc99410ab43d31
Edwin Roman <romanseberino@gmail.com>
2022-11-05 10:01:48 -0400

    Code refactor, more readable implmentation of the remove method

--- Data_Structure/C++/Tarea-linked-list/Lista.cpp
+++ Data_Structure/C++/Tarea-linked-list/Lista.cpp
@@ -7,23 +7,65 @@ Lista::Lista(): _primer(NULL) , _ultimo(NULL)
 
 }
 
+void Lista::Agregar(Elemento* elemento)
+{
+
+	/*IMPLEMENTE ESTE METODO
+		Dado un objeto de tipo puntero a Elemento agréguelo a la lista.
+		Tenga pendiente que el atributo _primer guarda un puntero al primer elemento de la lista y que _ultimo es un puntero al al último elemento de la lista
+	*/
+
+	if(_primer == NULL){
+		
+		_primer = elemento;
+		_ultimo = elemento;
+		
+	}else{
+
+		_ultimo->SetSiguiente(elemento);
+		_ultimo = elemento;
+	}
+
+	
+}
+
+
 
 bool Lista::Eliminar(int indice)
 {
+	/*
+	IMPLEMENTE ESTE METODO
+	Dado un indice entero elimine el objeto en esa posición.  Ejemplo si el parámetro indice es igual a 0 se eliminará el primer elemento.
+
+	Esta función retorna un booleano,  retorno verdadero si el índice está contenido en la lista.
+
+	Ejemplo, si la lista tiene 4 elementos y se pide eliminar el índice 9 este método retornará falso porque ese índice no está contenido en la lista.
+	*/
 
 	int inx = 0;
 
 	Elemento* elem = _primer;
 
+	// This while is to know how many element there are in the list.
 	while (elem != NULL)
 	{			
 		elem = elem->GetSiguiente();
 		inx++;
 	}
 
+
 	if(!(inx-1 >= indice))
 		return false;
+	/* 
+		These two condition mean the same if the given index is higher than our 
+		calculated index we get out of range. (The under condition look more simple).
+	 */
+	if(indice > inx)
+		return false;
 
+
+
+	//Este bloque busca el elemento en espefico dado el indice.
 	inx = 0;
 	
 	Elemento* previous = NULL;
@@ -43,57 +85,27 @@ bool Lista::Eliminar(int indice)
 	}
 	
 
-	if(current == NULL){
+	/* Refactor */
+
+	if(current == NULL)
 		return false;
-	}else{
+	
+
+	if(previous == NULL){ 
 		
-		if(previous != NULL){ 
-			previous->SetSiguiente(current->GetSiguiente());
+		if(_primer->GetSiguiente() == NULL){ 
+			_primer = NULL; //Cuando queda solo ese primer elemento y nada a que apuntar.
+			delete current;
+		}else{
+			_primer = _primer->GetSiguiente(); //Si estamos eliminando el primer elemento pero este tiene otro elementos al que apuntar.
 			delete current;
 		}
-		else{
-
-			if(_primer->GetSiguiente() != NULL){ 
-				_primer = _primer->GetSiguiente(); //Si estamos eliminando el primer elemento pero este tiene otro elementos al que apuntar.
-				delete current;
-			}else{
-				_primer = NULL; //Cuando queda solo ese primer elemento y nada a que apuntar.
-				delete current;
-			}
-
-		}
-		
-		return true;
 	}
-	
-	return false;
-
-	/*
-	IMPLEMENTE ESTE METODO
-	Dado un indice entero elimine el objeto en esa posición.  Ejemplo si el parámetro indice es igual a 0 se eliminará el primer elemento.
-
-	Esta función retorna un booleano,  retorno verdadero si el índice está contenido en la lista.
-
-	Ejemplo, si la lista tiene 4 elementos y se pide eliminar el índice 9 este método retornará falso porque ese índice no está contenido en la lista.
-	*/
-}
-
-void Lista::Agregar(Elemento* elemento)
-{
-
-	if(_primer == NULL){
-		
-		_primer = elemento;
-		_ultimo = elemento;
-		
-	}else{
-
-		_ultimo->SetSiguiente(elemento);
-		_ultimo = elemento;
+	else{
+		previous->SetSiguiente(current->GetSiguiente());
+		delete current;
 	}
+		
+	return true;
 
-	/*IMPLEMENTE ESTE METODO
-		Dado un objeto de tipo puntero a Elemento agréguelo a la lista.
-		Tenga pendiente que el atributo _primer guarda un puntero al primer elemento de la lista y que _ultimo es un puntero al al último elemento de la lista
-	*/
 }
